45ead29afa94bfdf5efd9369518708ea31dc3a1b
Yuanzhe Xu <yxu333@gatech.edu>
2016-03-23 14:08:31 -0400

    Working Interfaces

--- UnoInterface/UnoInterface.ino
+++ UnoInterface/UnoInterface.ino
@@ -11,6 +11,12 @@ const int SPEAKER = 6;
 // LED intensities
 int intensity1 = 0;
 int intensity2 = 0;
+// Note durations
+unsigned long duration1 = 0;
+unsigned long duration2 = 0;
+// Note starting times
+unsigned long start1 = 0;
+unsigned long start2 = 0;
 
 void setup() {
   pinMode(LED1, OUTPUT);
@@ -29,16 +35,40 @@ void loop() {
 void receiveEvent(int howMany) {
   byte note = Wire.read();
   byte velocity = Wire.read();
-  byte noteOn = Wire.read();
+  bool noteOn = Wire.read();
 
   if (note == 60) {
     intensity1 = map(velocity, 0, 127, 0, 255);
+    Serial.print("Intensity 1: ");
     Serial.println(intensity1);
     analogWrite(LED1, intensity1);
+
+    if (noteOn == true) {
+      start1 = millis();
+    }
+    else {
+      duration1 += millis() - start1;
+    }
+
+    Serial.print("Duration 1: ");
+    Serial.println(duration1);
+    Serial.println();
   }
   else {
     intensity2 = map(velocity, 0, 127, 0, 255);
+    Serial.print("Intensity 2: ");
     Serial.println(intensity2);
     analogWrite(LED2, intensity2);
+
+    if (noteOn == true) {
+      start2 = millis();
+    }
+    else {
+      duration2 += millis() - start2;
+    }
+
+    Serial.print("Duration 2: ");
+    Serial.println(duration2);
+    Serial.println();
   }
 }
